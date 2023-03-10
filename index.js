f1aa4527f54f3ec9fe0441d5e697ab5e2c09217f
Chetan Pandey <starkboy02@gmail.com>
2023-03-10 18:58:17 +0530

    fifth handson

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const socket = require('socket.io');
+const path = require('path');
+const PORT = 3001;
+
+const app = express();
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + './public/index.html');
+})
+
+const server = app.listen(PORT, function(){
+    console.log(`Application started at http://localhost:${PORT}`)
+});
+
+const io = socket(server, {cors: {origin : "*"}});
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        console.log('User Joined', name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('recieve', {message: message, name: users[socket.id]});
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+})
